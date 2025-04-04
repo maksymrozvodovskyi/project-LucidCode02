2fbc4cf9d5566f0de911a8d56d5e9dc4c00f07c1
Ihor <u96ok69@gmail.com>
2025-04-04 19:59:39 +0300

    fix css and js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,19 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const coversSection = document.getElementById('covers');
-  const coversList = document.querySelector('.covers-list');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const coversSection = document.getElementById('covers');
+//   const coversList = document.querySelector('.covers-list');
 
-  const observer = new IntersectionObserver(
-    entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          coversList.style.transform = 'translateX(0)';
-        } else {
-          coversList.style.transform = 'translateX(100%)';
-        }
-      });
-    },
-    { threshold: 0.3 }
-  );
+//   const observer = new IntersectionObserver(
+//     entries => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           coversList.style.transform = 'translateX(0)';
+//         } else {
+//           coversList.style.transform = 'translateX(100%)';
+//         }
+//       });
+//     },
+//     { threshold: 0.3 }
+//   );
 
-  observer.observe(coversSection);
-});
+//   observer.observe(coversSection);
+// });
+
+const section = document.querySelector('.section-covers');
+const covers = document.querySelectorAll('.cover');
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        covers.forEach((cover, i) => {
+          cover.style.animation = `marqueeLine 12s ease-in-out infinite ${
+            i * 0.1
+          }s`;
+        });
+        observer.unobserve(section);
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observer.observe(section);
