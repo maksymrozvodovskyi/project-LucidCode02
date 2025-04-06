fc39e97b4d60c73075946d0f4f9cc00e39d468df
maksymrozvodovskyi <152100542+maksymrozvodovskyi@users.noreply.github.com>
2025-04-07 00:53:16 +0300

    Update about-me.js

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,16 +1,1 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
-
-new Accordion('.accordion-container', {
-  duration: 400,
-  showMultiple: true,
-  openOnInit: [0],
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
-});
-// const accordion = new Accordion('.accordion-container');
-// accordion.open(0);
 
