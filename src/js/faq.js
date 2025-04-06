778836bf6498947b176cfa0a9c2da1ee8e952548
Roma3577 <mularr37@gmail.com>
2025-04-06 16:27:51 +0300

    sunday1

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,9 +5,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const accordion = new Accordion(".accordion-container");
   const panels = document.querySelectorAll('.ac');
   panels.forEach(panel => {
-    const trigger = panel.querySelector('.ac-trigger'); 
+    const trigger = panel.querySelector('.ac-trigger');
     trigger.addEventListener('click', function () {
       panel.classList.toggle('is-open');
     });
   });
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const items = document.querySelectorAll(".ac");
+
+//   items.forEach((item) => {
+//     const trigger = item.querySelector(".ac-trigger");
+
+//     trigger.addEventListener("click", () => {
+//       const isOpen = item.classList.contains("is-open");
+
+//       // Закриваємо всі інші
+//       items.forEach((i) => {
+//         i.classList.remove("is-open");
+//         i.querySelector(".ac-panel").style.maxHeight = null;
+//       });
+
+//       // Якщо панель не була відкрита — відкриваємо її
+//       if (!isOpen) {
+//         item.classList.add("is-open");
+//         const panel = item.querySelector(".ac-panel");
+//         panel.style.maxHeight = panel.scrollHeight + "px";
+//       }
+//     });
+//   });
+// });
