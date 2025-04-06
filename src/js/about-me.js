4959bbb92f1efeba23cb354fb9ee6c9d5797c4c5
Sergeyt2000 <sergeyt2000@gmail.com>
2025-04-06 21:33:54 +0300

    About me with Accordion & Swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 
 new Accordion('.accordion-container', {
   duration: 400,
@@ -14,3 +14,38 @@ new Accordion('.accordion-container', {
 // const accordion = new Accordion('.accordion-container');
 // accordion.open(0);
 
+const swiper = new Swiper('.about-me-swiper-box', {
+  direction: 'horizontal',
+  modules: [Navigation, Keyboard],
+  navigation: {
+    nextBtnEl: '.about-me-left-arrow-btn',
+  },
+  keyboard: {
+    enabled: true,
+  },
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      allowTouchMove: true,
+    },
+    768: {
+      slidesPerView: 3,
+      allowTouchMove: true,
+    },
+    1440: {
+      slidesPerView: 6,
+      allowTouchMove: false,
+    },
+  },
+});
+
+const nextBtnEl = document.querySelector(
+  '.about-me-left-arrow-btn'
+);
+nextBtnEl.addEventListener('click', () => {
+  swiper.slideNext();
+});
+
+// const swiper = document.querySelector('.about-me-swiper').swiper;
+// swiper.slideNext();
