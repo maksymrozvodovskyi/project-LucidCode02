c335d17e7283da592cfc29530f10cc911254af8d
Bohdan <toyvo7@gmail.com>
2025-04-08 21:13:20 +0200

    add modal & js-header, modify header

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,41 @@
-`user strict`;
+// `use strict`;
 
 const headerNav = document.querySelector('.header-nav-list');
-const headerMenuNav = document.querySelector('.header-nav-button');
+const headerMenuNav = document.querySelector('.js-menu-header');
+const openModalBtn = document.querySelector('.js-menu-open');
+const closeModalBtn = document.querySelector('.js-menu-close');
+const modal = document.querySelector('.js-menu');
+const closeOnElements = document.querySelectorAll('.modal-nav-item, .modal-nav-order');
+
+
 
 headerMenuNav.addEventListener('click', () => {
-    headerNav.classList.toggle('visually-hidden');
+   headerNav.classList.toggle('visually-hidden');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (openModalBtn) {
+    openModalBtn.addEventListener('click', () => {
+     
+      if (modal) {
+        modal.classList.add('is-open');
+      }
+    });
+  }
+
+  if (closeModalBtn) {
+    closeModalBtn.addEventListener('click', () => {
+      if (modal) {
+        modal.classList.remove('is-open');
+      }
+    });
+  }
+
+  closeOnElements.forEach(element => {
+    element.addEventListener('click', () => {
+      if (modal) {
+        modal.classList.remove('is-open');
+      }
+    });
+  });
 });
