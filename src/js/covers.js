423eb4a34d8f4f367606af03b459b324838b9cad
Ihor <u96ok69@gmail.com>
2025-04-09 07:26:11 +0300

    optimize img

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,31 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const evenRows = document.querySelectorAll('.row--even');
-//   const oddRows = document.querySelectorAll('.row--odd');
-//   const section = document.querySelector('.section-covers');
-
-//   const observerOptions = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 0.25,
-//   };
-
-//   const observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         evenRows.forEach(row => row.classList.add('animate'));
-//         oddRows.forEach(row => row.classList.add('animate'));
-//       } else {
-//         evenRows.forEach(row => row.classList.remove('animate'));
-//         oddRows.forEach(row => row.classList.remove('animate'));
-//       }
-//     });
-//   }, observerOptions);
-
-//   if (section) {
-//     observer.observe(section);
-//   }
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
   const section = document.querySelector('.section-covers');
   const evenRows = document.querySelectorAll('.row--even');
