ffe71ed3e2e410e3dcdeae7868c20f91b0134c84
Roma3577 <mularr37@gmail.com>
2025-04-09 20:35:38 +0300

    attempt

--- src/js/faq.js
+++ src/js/faq.js
@@ -9,13 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
       closeOthers: false,
     });
   });
-  const panels = document.querySelectorAll(".ac");
-  panels.forEach((panel) => {
-    const trigger = panel.querySelector(".ac-trigger");
-    trigger.addEventListener("click", () => {
-      panel.classList.toggle("is-open", panel.classList.contains("is-active"));
-    });
-  });
 });
 
 
