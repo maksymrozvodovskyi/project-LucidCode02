1a2b49a272be976e11453a065bc007f1f1b72b31
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-04-09 18:24:32 +0300

    fix a litl bit of problems

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,43 +1,37 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-import { Navigation,Keyboard } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 
+const swiper = new Swiper('.swiper-box .swiper', {
+  modules: [Navigation, Keyboard],
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 20,
 
+  navigation: {
+    nextEl: '.slider-arrow-next',
+    prevEl: '.slider-arrow-prev',
+  },
 
-const swiper = new Swiper('.swiper-box .swiper',
-    {
-      modules: [Navigation, Keyboard],
-      direction: 'horizontal',
-      slidesPerView: 1,
-      spaceBetween: 20,
+  keyboard: {
+    enabled: true,
+  },
+});
 
-      navigation: {
-        nextEl: '.slider-arrow-next',
-        prevEl: '.slider-arrow-prev',
-      },
+let isInVPr = false;
+const observerProjectsSection = new IntersectionObserver(
+  ([entry]) => {
+    isInVPr = entry.isIntersecting;
+  },
+  { threshold: 0.5 }
+);
 
-      keyboard: {
-        enabled: true
-      }
-
-
-    });
-
-    let isInVPr = false;
-    const observerProjectsSection = new IntersectionObserver(
-    ([entry]) => {
-        isInVPr = entry.isIntersecting;
-    },
-    { threshold: 0.5 }
-    );
-
-    const projectsSwiperElement = document.querySelector('.swiper-box');
-    observerProjectsSection.observe(projectsSwiperElement);
-    document.addEventListener('keydown',  (evt) => {
-        if (!isInVPr) return;
-        if (evt.key === 'Tab') {
-            evt.preventDefault();
-            swiper.slideNext();
-        }
-
-    });
+const projectsSwiperElement = document.querySelector('.swiper-box');
+observerProjectsSection.observe(projectsSwiperElement);
+document.addEventListener('keydown', evt => {
+  if (!isInVPr) return;
+  if (evt.key === 'Tab') {
+    evt.preventDefault();
+    swiper.slideNext();
+  }
+});
