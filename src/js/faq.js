e006a8d9763abf12f79be592d0d81f8f8f244b38
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-04-11 13:46:59 +0300

    fix section faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,14 +1,28 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+import Accordion from 'accordion-js';
+new Accordion('.accordion-container-faq', {
+  duration: 400,
+  showMultiple: true,
+});
 
-document.addEventListener("DOMContentLoaded", function () {
-  const containers = document.querySelectorAll(".accordion-container");
-  containers.forEach((container) => {
-    new Accordion(container, {
-      showMultiple: true,
-      closeOthers: false,
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionItems = document.querySelectorAll('.faq-item');
+  faqs.forEach(faq =>
+    faq.addEventListener('click', () => {
+      faq.classList.toggle('active');
+    })
+  );
+
+  accordionItems.forEach(item => {
+    const btn = item.querySelector('.faq-btn');
+    const content = item.querySelector('.ac-panel');
+
+    btn.addEventListener('click', function () {
+      if (content.style.display === 'flex') {
+        content.style.overflow = 'hidden';
+      } else {
+        content.style.display = 'flex';
+      }
     });
   });
 });
-
-
+const faqs = document.querySelectorAll('.faq-item');
